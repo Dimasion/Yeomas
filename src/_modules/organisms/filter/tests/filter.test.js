86f417281395f9e9c16a1423c00faba334f918bb
Dimasion <dimasion2010@gmail.com>
2017-07-11 15:12:44 +0300

    Added filter (70%). Added button.

---
+++ src/_modules/organisms/filter/tests/filter.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import Filter from '../filter';
+
+describe('Filter View', function() {
+
+  beforeEach(() => {
+    this.filter = new Filter();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.filter);
+  });
+
+});
